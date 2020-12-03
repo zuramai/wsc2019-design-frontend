a40bd21063a5f1363bd625abf0b4ce09ad117303
zuramai <ahmadsaugigis@yahoo.com>
2020-12-03 14:29:50 +0700

    Added modal functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,8 @@ const app = document.getElementById('app')
 const element_dummy = document.querySelector('.element')
 const distance = 150;
 const canvas = document.getElementById('canvas');
-
+const modalWrapper = document.getElementById('modal-wrapper')
+const ctx = canvas.getContext('2d')
 let lines = []
 initCanvas()
 
@@ -10,6 +11,10 @@ let elements = [
     {
         x: document.body.clientWidth / 2,
         y: document.body.clientHeight / 2,
+        content: {
+            text: '',
+            connectedNumbers: []
+        }
     }
 ];
 
@@ -17,10 +22,6 @@ let elements = [
 elements.forEach((element,index) => {
     let newEl = drawElement(element)
     app.appendChild(newEl)
-});
-lines.forEach((line,index) => {
-    let newLine = drawLine(line)
-    app.appendChild(newLine)
 });
 
 /**
@@ -43,56 +44,27 @@ function drawElement(element) {
  */
 function drawLine(line) {
 
-    let lineWrapper = document.createElement('div');
-    lineWrapper.classList.add("line-wrapper");
-    
-    let newLine = document.createElement('div')
-    newLine.classList.add("line");
-    
-    let fromElementLocation = line.from.element.getBoundingClientRect();
-    let toElementLocation = line.to.element.getBoundingClientRect();
+    let fromRect = line.from.element.getBoundingClientRect();
+    let toRect = line.to.element.getBoundingClientRect();
+    let fromLocation, toLocation;
 
-    console.log(fromElementLocation, toElementLocation)
-    let from,to;
-        console.log(line)
-    if(line.from.number == 1) from = { x: fromElementLocation.x +fromElementLocation.width/2 , y: toElementLocation.y + toElementLocation.height }
-    else if(line.from.number == 2) from = { x: fromElementLocation.x + fromElementLocation.width, y: fromElementLocation.y + fromElementLocation.height/2 }
-    else if(line.from.number == 3) from = { x: fromElementLocation.x + fromElementLocation.width/2, y: fromElementLocation.y + fromElementLocation.height }
-    else from = { x: toElementLocation.x + toElementLocation.width, y: fromElementLocation.y + fromElementLocation.height/2 }
- 
-    if(line.to.number == 1) to = { x: toElementLocation.x, y: toElementLocation.y }
-    else if(line.to.number == 2) to = { x: fromElementLocation.x, y: toElementLocation.y }
-    else if(line.to.number == 3) to = { x: toElementLocation.x + toElementLocation.width/2, y: toElementLocation.y + toElementLocation.height/2-10 }
-    else to = { x: toElementLocation.x, y: toElementLocation.y }
+    if(line.from.number == 1) fromLocation = {x: fromRect.x + fromRect.width/2, y: fromRect.y}
+    else if(line.from.number == 2) fromLocation = {x: fromRect.x + fromRect.width, y: fromRect.y + fromRect.height/2}
+    else if(line.from.number == 3) fromLocation = {x: fromRect.x + fromRect.width/2, y: fromRect.y + fromRect.height}
+    else if(line.from.number == 4) fromLocation = {x: fromRect.x, y: fromRect.y + fromRect.height/2}
 
-    console.log(line)
+    if(line.to.number == 1) toLocation = {x: toRect.x + toRect.width/2, y: toRect.y}
+    else if(line.to.number == 2) toLocation = {x: toRect.x + toRect.width, y: toRect.y + toRect.height/2}
+    else if(line.to.number == 3) toLocation = {x: toRect.x + toRect.width/2, y: toRect.y + toRect.height}
+    else if(line.to.number == 4) toLocation = {x: toRect.x, y: toRect.y + toRect.height/2}
 
-    if(["1",'3'].includes(line.from.number)) {
-        lineWrapper.style.height = Math.abs(to.y - from.y)+'px';
-        lineWrapper.classList.add("vertical")
-    }else{
-        lineWrapper.style.width = Math.abs(to.x - from.x)+'px';
-        lineWrapper.classList.add("horizontal")
-    }
-    lineWrapper.style.top = from.y+'px';
-    lineWrapper.style.left = from.x+'px';
-
-
-    lineWrapper.appendChild(newLine)
-
-    return lineWrapper;
+    ctx.strokeStyle = "#000"
+    ctx.beginPath();
+    ctx.moveTo(fromLocation.x, fromLocation.y);
+    ctx.lineTo(toLocation.x, toLocation.y);
+    ctx.stroke()
 }
-document.querySelectorAll('.element').forEach((element,index) => {
-    let numbers = element.querySelectorAll('.number');
-    let edit = element.querySelector(".edit");
 
-    edit.addEventListener("click", (e) => {
-        editContent(e, index);
-    });
-    numbers.forEach((number) => {
-        number.addEventListener('click', (e) => newElement(e, element));
-    });
-})
 
 function newElement(event, element) {
     let numberClicked = event.target.getAttribute('data-number');
@@ -126,6 +98,16 @@ function newElement(event, element) {
             y: elementLocation.y + elementLocation.height/2
         }        
     }
+    
+    createElementLocation.content = {
+        text: '',
+        connectedNumbers: [
+            {
+                number: numberClicked,
+                buttonText: ''
+            }
+        ]
+    };
     let createdElement = createElement(createElementLocation);
     createLine({
         from: {
@@ -137,6 +119,11 @@ function newElement(event, element) {
             number: connectedNumber,
         }   
     });
+    elements[elements.length-2].content.connectedNumbers.push({
+        number: connectedNumber,
+        buttonText: ''
+    })
+
 }
 
 /**
@@ -146,13 +133,18 @@ function newElement(event, element) {
 function createElement(el) {
     elements.push({
         x: el.x,
-        y: el.y
+        y: el.y,
+        content: el.content
     });
     let newEl = drawElement(el)
     newEl.setAttribute("id", `element-${countElement()}`);
     app.appendChild(newEl)
 
     let numbers = newEl.querySelectorAll('.number');
+    let edit = newEl.querySelector('.edit');
+    edit.addEventListener("click", (e) => {
+        editContent(e, elements.length-1);
+    });
     numbers.forEach((number) => {
         number.addEventListener('click', (e) => newElement(e, newEl));
     });
@@ -161,19 +153,40 @@ function createElement(el) {
 }
 function createLine(line) {
     lines.push(line);
-    let newLine = drawLine(line);
-    app.appendChild(newLine)
+    drawLine(line)
 }
 
 function countElement() {
     return document.querySelectorAll(".element").length
 }
 
+/**
+ * Edit Content Callback
+ * @param {Event} event 
+ * @param {Number} index  
+ */
 function editContent(event, index) {
-    console.log('edit content')
+    modalWrapper.style.display = "flex";
 }
 
+/**
+ * Create initial settings for canvas
+ */
 function initCanvas() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 }
+document.querySelectorAll('.element').forEach((element,index) => {
+    let numbers = element.querySelectorAll('.number');
+    let edit = element.querySelector(".edit");
+
+    edit.addEventListener("click", (e) => {
+        editContent(e, index);
+    });
+    numbers.forEach((number) => {
+        number.addEventListener('click', (e) => newElement(e, element));
+    });
+})
+document.querySelector(".close").addEventListener('click', () => {
+    modalWrapper.style.display = 'none';
+})
